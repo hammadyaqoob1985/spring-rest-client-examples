5d3fd5e10451d3addf5d72229c5208f5dde08b88
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2018-12-28 15:08:58 +0000

    changed domains classes as per example using json to pojo converter

--- src/main/java/guru/springframework/api/domain/Billing.java
+++ src/main/java/guru/springframework/api/domain/Billing.java
@@ -1,9 +1,29 @@
 package guru.springframework.api.domain;
 
-import lombok.Data;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
-@Data
-public class Billing {
+public class Billing implements Serializable {
 
     private Card card;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    private final static long serialVersionUID = 6577338081290507077L;
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
 }
