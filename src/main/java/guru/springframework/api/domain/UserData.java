abc706610a372d41060911cad6f5b2721cc319f4
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2018-12-28 15:46:20 +0000

    Successfully consumed api service

---
+++ src/main/java/guru/springframework/api/domain/UserData.java
@@ -0,0 +1,17 @@
+package guru.springframework.api.domain;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class UserData implements Serializable {
+
+    private List<User> data;
+
+    public List<User> getData() {
+        return data;
+    }
+
+    public void setData(List<User> data) {
+        this.data = data;
+    }
+}
